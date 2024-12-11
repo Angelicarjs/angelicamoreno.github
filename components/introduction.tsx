602b99ce6cd49960b2f445fe9949e4f6612b1ed1
Angelicarjs <angelica_uno@hotmail.es>
2024-12-11 03:44:39 -0500

    introduction updated to deploy

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -5,20 +5,18 @@ const Introduction = () => {
     return (
         <div className="z-20 w-full bg-darkBg/60">
             <div className="z-20 grid items-center h-full p-6 py-20 md:py-0 md:grid-cols-2">
-            <div className="place-self-center">
-                <Image src="/home-4.png" priority width="300" height="300" alt="Avatar" />
-            </div>
+                <Image src="/home-4.png" priority width="800" height="800" alt="Avatar" />
                 <div className="flex flex-col justify-center max-w-md">
-                    <h1 className="mb-5 text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-10">If you can think it, <br />
+                    <h1 className="mb-5 text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-10">Si puedes pensarlo, <br />
                         <TypeAnimation
                             sequence={[
-                                'you can develop it',
+                                'puedes programarlo',
                                 1000,
-                                'you can optimize it',
+                                'puedes optimizarlo',
                                 1000,
-                                'you can implement it',
+                                'puedes implementarlo',
                                 1000,
-                                'you can program it',
+                                'puedes desarrollarlo',
                                 1000
                             ]}
                             wrapper="span"
@@ -29,16 +27,16 @@ const Introduction = () => {
                     </h1>
 
                     <p className="mx-auto mb-2 text-xl md:text-xl md:mx-0 md:mb-8">
-                    Geoscientist and master's student in Digital Earth, specializing in Data Sciences and Artificial Intelligence. Passionate about uncovering insights from geographic data through advanced data analysis and AI-driven approaches.
+                        Como desarrollador frontend y creador de contenido, me dedico a combinar diseño y funcionalidad para crear experiencias digitales impactantes y accesibles.
                     </p>
 
                     <div className="flex items-center justify-center gap-3 md:justify-start md:gap-10">
                         <a href="/projects" className="px-3 py-2 my-2 transition-all border-2 cursor-pointer text-md w-fit rounded-xl hover:shadow-xl hover:shadow-white/50">
-                            Projects
+                            Ver proyectos
                         </a>
                         <a href="/contact"
                             className="px-3 py-2 my-5 transition-all border-2 cursor-pointer text-md w-fit text-secondary border-secondary rounded-xl hover:shadow-xl hover:shadow-secondary" >
-                            Contact me
+                            Contacta conmigo
                         </a>
                     </div>
                 </div>
