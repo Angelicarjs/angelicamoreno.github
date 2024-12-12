f435ef926cc26ea4e5623d0245247abcecf52855
Angelicarjs <angelica_uno@hotmail.es>
2024-12-12 11:53:30 -0500

    introduction changes2

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -28,15 +28,16 @@ const Introduction = () => {
 
                     <p className="mx-auto mb-2 text-xl md:text-xl md:mx-0 md:mb-8">
                         Como desarrollador frontend y creador de contenido, me dedico a combinar diseño y funcionalidad para crear experiencias digitales impactantes y accesibles.
+                        Geoscientist and master's student in Digital Earth, specializing in Data Sciences and Artificial Intelligence. Passionate about uncovering insights from geographic data through advanced data analysis and AI-driven approaches.
                     </p>
 
                     <div className="flex items-center justify-center gap-3 md:justify-start md:gap-10">
                         <a href="/projects" className="px-3 py-2 my-2 transition-all border-2 cursor-pointer text-md w-fit rounded-xl hover:shadow-xl hover:shadow-white/50">
-                            Ver proyectos
+                            Projects
                         </a>
                         <a href="/contact"
                             className="px-3 py-2 my-5 transition-all border-2 cursor-pointer text-md w-fit text-secondary border-secondary rounded-xl hover:shadow-xl hover:shadow-secondary" >
-                            Contacta conmigo
+                            Contact me
                         </a>
                     </div>
                 </div>
