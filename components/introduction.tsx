7cb2b8a3792f115918b76454c161ae0c6fecaa2e
Angelicarjs <angelica_uno@hotmail.es>
2024-12-12 11:23:32 -0500

    re-start intrduction for deploy

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -5,9 +5,11 @@ const Introduction = () => {
     return (
         <div className="z-20 w-full bg-darkBg/60">
             <div className="z-20 grid items-center h-full p-6 py-20 md:py-0 md:grid-cols-2">
+            <div className="place-self-center">
                 <Image src="/home-4.png" priority width="300" height="300" alt="Avatar" />
+            </div>
                 <div className="flex flex-col justify-center max-w-md">
-                    <h1 className="mb-5 text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-10">Si puedes pensarlo, <br />
+                    <h1 className="mb-5 text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-10">If you can think it, <br />
                         <TypeAnimation
                             sequence={[
                                 'you can develop it',
@@ -27,7 +29,7 @@ const Introduction = () => {
                     </h1>
 
                     <p className="mx-auto mb-2 text-xl md:text-xl md:mx-0 md:mb-8">
-                        Geoscientist and master's student in Digital Earth, specializing in Data Sciences and Artificial Intelligence. Passionate about uncovering insights from geographic data through advanced data analysis and AI-driven approaches.
+                    Geoscientist and master's student in Digital Earth, specializing in Data Sciences and Artificial Intelligence. Passionate about uncovering insights from geographic data through advanced data analysis and AI-driven approaches.
                     </p>
 
                     <div className="flex items-center justify-center gap-3 md:justify-start md:gap-10">
@@ -36,7 +38,7 @@ const Introduction = () => {
                         </a>
                         <a href="/contact"
                             className="px-3 py-2 my-5 transition-all border-2 cursor-pointer text-md w-fit text-secondary border-secondary rounded-xl hover:shadow-xl hover:shadow-secondary" >
-                            Contacta conmigo
+                            Contact me
                         </a>
                     </div>
                 </div>
