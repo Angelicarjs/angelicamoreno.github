48c03ce511aa3d157cf19596fa91ff97329212ac
Angelicarjs <angelica_uno@hotmail.es>
2024-12-12 11:37:24 -0500

    introduction changes1

--- components/introduction.tsx
+++ components/introduction.tsx
@@ -5,7 +5,7 @@ const Introduction = () => {
     return (
         <div className="z-20 w-full bg-darkBg/60">
             <div className="z-20 grid items-center h-full p-6 py-20 md:py-0 md:grid-cols-2">
-                <Image src="/home-4.png" priority width="800" height="800" alt="Avatar" />
+                <Image src="/home-4.png" priority width="300" height="300" alt="Avatar" />
                 <div className="flex flex-col justify-center max-w-md">
                     <h1 className="mb-5 text-2xl leading-tight text-center md:text-left md:text-4xl md:mb-10">Si puedes pensarlo, <br />
                         <TypeAnimation
@@ -27,7 +27,7 @@ const Introduction = () => {
                     </h1>
 
                     <p className="mx-auto mb-2 text-xl md:text-xl md:mx-0 md:mb-8">
-                        Como desarrollador frontend y creador de contenido, me dedico a combinar diseño y funcionalidad para crear experiencias digitales impactantes y accesibles.
+                    Geoscientist and master's student in Digital Earth, specializing in Data Sciences and Artificial Intelligence. Passionate about uncovering insights from geographic data through advanced data analysis and AI-driven approaches.
                     </p>
 
                     <div className="flex items-center justify-center gap-3 md:justify-start md:gap-10">
