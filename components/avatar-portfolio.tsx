e2389d5fae1e82d8de393ea4bc843c069686a119
Angelicarjs <angelica_uno@hotmail.es>
2024-12-12 13:54:41 -0500

    logos insertion

--- components/avatar-portfolio.tsx
+++ components/avatar-portfolio.tsx
@@ -6,9 +6,17 @@ import { MotionTransition } from './transition-component';
 
 const AvatarPortfolio = () => {
     return (
-        <MotionTransition position="bottom" className="bottom-0 right-0 hidden md:inline-block md:absolute ">
-            <Image src="/uni-sbg-logo-white.png" width="100" height="200" className="w-full h-full " alt="Particles " />
+        <MotionTransition position="bottom" className="fixed bottom-0 left-0 right-0 flex justify-center">
+            <Image 
+                src="/funded_EU.jpg" 
+                width={300} 
+                height={100} 
+                className="w-24 h-auto" 
+                alt="Particles" 
+            />
         </MotionTransition>
+
+
 
     );
 }
--- components/avatar.tsx
+++ components/avatar.tsx
@@ -8,7 +8,7 @@ import { MotionTransition } from "./transition-component";
 export function Avatar() {
     return (
         <MotionTransition position="bottom" className="bottom-0 right-0 hidden md:inline-block md:absolute ">
-            <Image src="/uni-sbg-logo-white.png" width="100" height="200" className="w-full h-full " alt="Particles " />
+            <Image src="/plus_logo.jpg" width="100" height="200" className="w-full h-full " alt="Particles " />           
         </MotionTransition>
     )
 }
